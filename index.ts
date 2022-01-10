8791cd2fe63466946b5f9e77be202cbb2957a512
Riceblades11 <96640977+Riceblade@users.noreply.github.com>
2022-01-09 21:43:09 -0500

    added images

--- index.ts
+++ index.ts
@@ -9,9 +9,9 @@ client.on('ready', () => {
 })
     client.on('messageCreate', (message) => {
         if (message.content === '<@!801336408077238272>') {
-            message.reply("You purchased God Potion for 300,000,000 coins! <@!801336408077238272>")
-            message.reply("You purchased God Potion for 700,000,000 coins! <@!801336408077238272>")
-            message.reply("You died and lost 517,771,801.4 coins! <@!801336408077238272>")
+            message.reply("You purchased God Potion for 300,000,000 coins! https://cdn.discordapp.com/attachments/920113587463266315/926938976546340864/unknown.png <@!801336408077238272>")
+            message.reply("You purchased God Potion for 700,000,000 coins! https://cdn.discordapp.com/attachments/920113587463266315/926933351766585384/unknown.png <@!801336408077238272>")
+            message.reply("You died and lost 517,771,801.4 coins! https://cdn.discordapp.com/attachments/920113587463266315/926933094542504056/unknown.png <@!801336408077238272>")
             message.react('<:zingel:924360562652114974>')
         }
     })
